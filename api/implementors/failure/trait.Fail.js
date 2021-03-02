fd6ccdbdb5d006c234abeb8182794b48395e0fb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-02 14:57:37 +0000

    Deploy chinedufn/landon to github.com/chinedufn/landon.git:gh-pages

---
+++ api/implementors/failure/trait.Fail.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["blender_mesh"] = [{"text":"impl Fail for <a class=\"enum\" href=\"blender_mesh/enum.FlattenMeshError.html\" title=\"enum blender_mesh::FlattenMeshError\">FlattenMeshError</a>","synthetic":false,"types":["blender_mesh::export::FlattenMeshError"]},{"text":"impl Fail for <a class=\"enum\" href=\"blender_mesh/enum.BlenderError.html\" title=\"enum blender_mesh::BlenderError\">BlenderError</a>","synthetic":false,"types":["blender_mesh::BlenderError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
